d8b3b7d253a7326590cf106deaa0918e4f1f01a4
Akshata Sabale <99897062+Akshata72@users.noreply.github.com>
2022-06-13 23:55:47 +0530

    [Akshata]Refactored : Add User

--- FundooNotes/BussinessLayer/Services/UserBL.cs
+++ FundooNotes/BussinessLayer/Services/UserBL.cs
@@ -25,41 +25,5 @@ namespace BussinessLayer.Services
                 throw;
             }
         }
-
-        public bool ForgetPassword(string Email)
-        {
-            try
-            {
-                return this.userRL.ForgetPassword(Email);
-            }
-            catch(Exception)
-            {
-                throw;
-            }
-        }
-
-        public string LoginUser(string Email, string Password)
-        {
-            try
-            {
-                return this.userRL.LoginUser(Email, Password);
-            }
-            catch(Exception)
-            {
-                throw;
-            }
-        }
-
-        public bool ResetPassword(string Email, PasswordModel passwordModel)
-        {
-            try
-            {
-                return this.userRL.ResetPassword(Email, passwordModel);
-            }
-            catch(Exception)
-            {
-                throw;
-            }
-        }
     }
 }

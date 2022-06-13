808d61f2ebda689e7c0811abb449e728374a8ded
Akshata Sabale <99897062+Akshata72@users.noreply.github.com>
2022-06-14 00:57:26 +0530

    [Akshata]Refactored : Update Note

--- FundooNotes/RepositoryLayer/Interfaces/INoteRL.cs
+++ FundooNotes/RepositoryLayer/Interfaces/INoteRL.cs
@@ -12,13 +12,6 @@ namespace RepositoryLayer.Interfaces
         Task AddNote(int UserId,NotePostModel notePostModel);
         Task DeleteNote(int UserId,int NoteId);
         Task UpdateNote(int UserId,int NoteId, UpdateModel updateModel);
-        Task IsTrash(int UserId,int NoteId);
-        Task Remainder(int UserId,int NoteId, DateTimeModel dateTimeModel);
-        Task IsArchive(int UserId,int NoteId);
-        Task IsPin(int UserId,int NoteId);
-        Task ChangeColour(int UserId,int NoteId,string Colour);
-        Task<Note> GetNote(int UserId,int NoteId);
-        Task<List<Note>> GetAllNotes(int UserId);
     }
 }
 

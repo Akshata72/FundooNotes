e38aebd1e697ba9dba0506057594239fc054a8fe
Akshata Sabale <99897062+Akshata72@users.noreply.github.com>
2022-06-14 00:47:16 +0530

    [Akshata]Refactored : Add Note

--- FundooNotes/BussinessLayer/Interfaces/INoteBL.cs
+++ FundooNotes/BussinessLayer/Interfaces/INoteBL.cs
@@ -11,14 +11,5 @@ namespace BussinessLayer.Interfaces
     public interface INoteBL
     {
         Task AddNote(int UserId, NotePostModel notePostModel);
-        Task DeleteNote(int UserId, int NoteId);
-        Task UpdateNote(int UserId, int NoteId, UpdateModel updateModel);
-        Task IsTrash(int UserId,int NoteId);
-        Task Remainder(int UserId, int NoteId, DateTimeModel dateTimeModel);
-        Task IsArchive(int UserId, int NoteId);
-        Task IsPin(int UserId, int NoteId);
-        Task ChangeColour(int UserId, int NoteId, string Colour);
-        Task<Note> GetNote(int UserId, int NoteId);
-        Task<List<Note>> GetAllNotes(int UserId);
     }
 }
